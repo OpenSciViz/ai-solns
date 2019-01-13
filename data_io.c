3a4cd596193863a18c801be391e93eb19a30dfd4
davidbhon <davidb.hon@gmail.com>
2019-01-12 20:59:02 -0800

    tested all io

--- data_io.c
+++ data_io.c
@@ -4,6 +4,7 @@
 #include "string.h"
 
 char** alloc_tiles(const int dim) {
+  // alloc 1 extra char for null termination of each string
   char** tiles = (char**) calloc(dim+1, sizeof(char*));;
   int y = 0;
   while( y < dim ) {
@@ -12,42 +13,7 @@ char** alloc_tiles(const int dim) {
   return tiles;
 }
 
-size_t read_tiles(const int dim, const char* filename, char** tiles) {
-  if( tiles == NULL ) tiles = alloc_tiles(dim);
-  FILE* fp = fopen(filename, "r+");
-  size_t nr = 0, y = 0;
-  char* s = ".";
-
-  while( y < dim && s != NULL  ) {
-    s = fgets(tiles[y], dim, fp);
-    nr += strlen(s) ; y += 1; // C src should be readily ported to python , so no y++ 
-  }
-  return nr;  //sizeof(tiles)
-}
-
-size_t write_tiles(const int dim, const char* filename, char** tiles) {
-  FILE* fp = fopen(filename, "r+");
-  size_t nw = 0, y = 0;
-
-  while( y < dim ) {
-    nw += fputs(tiles[y], fp);
-    y += 1; // C src should be readily ported to python , so no y++ 
-  }
-  return nw; //sizeof(tiles);
-}
-
-size_t set_tiles(const int dim, int c, char** tiles) {
-  size_t ns = 0, y = 0;
-
-  while( y < dim ) {
-    memset(tiles[y], c, dim*sizeof(char));
-    y += 1; // C src should be readily ported to python , so no y++ 
-    ns += dim;
-  }
-  return ns; //sizeof(tiles);
-}
-
-size_t print_tiles(const int dim, char** tiles) {
+size_t print_tiles(char** tiles, const int dim) {
   size_t np = 0, y = 0;
 
   while( y < dim ) {
@@ -57,12 +23,52 @@ size_t print_tiles(const int dim, char** tiles) {
   return np; //sizeof(tiles);
 }
 
+size_t read_tiles(char** tiles, const char* filename, const int dim) {
+  if( ! tiles ) tiles = alloc_tiles(dim);
+  FILE* fp = fopen(filename, "r");
+  if( ! fp ) return -1;
+
+  char* s = ".";
+  size_t nr = 0, y = 0;
+  while( y < dim && s != NULL  ) {
+    s = fgets(tiles[y], dim, fp);
+    nr += strlen(s) ; y += 1; // C src should be readily ported to python , so no y++ 
+  }
+  fclose(fp);
+  return nr;  //sizeof(tiles)
+}
+
+size_t write_tiles(char** tiles, const char* filename, const int dim) {
+  FILE* fp = fopen(filename, "w");
+  if( ! fp ) return -1;
+
+  size_t nw = 0, y = 0;
+  while( y < dim ) {
+    nw += fputs(tiles[y], fp);
+    y += 1; // C src should be readily ported to python , so no y++ 
+  }
+  fclose(fp);
+  return nw; //sizeof(tiles);
+}
+
+size_t set_tiles(char** tiles, int c, const int dim) {
+  size_t ns = 0, y = 0;
+
+  while( y < dim ) {
+    memset(tiles[y], c, (dim-1)*sizeof(char));
+    y += 1; // C src should be readily ported to python , so no y++ 
+    ns += dim;
+  }
+  print_tiles(tiles, dim);
+  return ns; //sizeof(tiles);
+}
+
 int main(int argc, char** args) {
   const int dim = 14;
   char** tiles = alloc_tiles(dim);
-  size_t ns = set_tiles(dim, '.', tiles);
-  size_t nw = write_tiles(dim, "tiles14x14.txt", tiles);
-  size_t nr = read_tiles(dim, "tiles14x14.txt", tiles);
-  print_tiles(dim, tiles);
+  size_t ns = set_tiles(tiles, '.', dim);
+  size_t nw = write_tiles(tiles, "tiles14x14.txt", dim);
+  size_t nr = read_tiles(tiles, "tiles14x14.txt", dim);
+  print_tiles(tiles, dim);
 }
 
